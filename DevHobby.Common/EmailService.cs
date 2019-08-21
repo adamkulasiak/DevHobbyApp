fd2cf2d766554cd67ca40e2254ed229a65c2667d
adam.kulasiak <adam.kulasiak@gmail.com>
2019-08-21 20:34:12 +0200

    Added classes Dostawca, LogowanieService, EmailService

---
+++ DevHobby.Common/EmailService.cs
@@ -0,0 +1,23 @@
+﻿namespace DevHobby.Common
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// Wysyla wiadomosc email
+        /// </summary>
+        /// <param name="temat">Temat wiadomosci</param>
+        /// <param name="wiadomosc">Wiadomosc tekstowa</param>
+        /// <param name="odbiorca">Adres email odbiorcy wiadomosci</param>
+        /// <returns></returns>
+        public string WyslijWiadomosc(string temat, string wiadomosc, string odbiorca)
+        {
+            // kod, aby wyslac wiadomosc email
+
+            var potwierdzenie = "Wiadomosc wyslana: " + temat;
+            var logowanieService = new LogowanieService();
+            logowanieService.Logowanie(potwierdzenie);
+
+            return potwierdzenie;
+        }
+    }
+}
