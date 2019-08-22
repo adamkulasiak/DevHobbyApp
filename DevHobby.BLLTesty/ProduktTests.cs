4852048f315c5e5f4ab1aa7aaa7874e6372cce15
adam.kulasiak <adam.kulasiak@gmail.com>
2019-08-22 18:44:30 +0200

    added produkt class

---
+++ DevHobby.BLLTesty/ProduktTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevHobby.DLL.Tests
+{
+    [TestClass()]
+    public class ProduktTests
+    {
+        [TestMethod()]
+        public void PowiedzWitajTest()
+        {
+            //arrange
+            var produkt = new Produkt();
+            produkt.ProduktId = 1;
+            produkt.NazwaProduktu = "Biurko";
+            produkt.Opis = "Czerwone biurko";
+            var oczekiwana = "Witaj Biurko(1): Czerwone biurko";
+
+            //act
+            var aktualna = produkt.PowiedzWitaj();
+
+            //assert
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+    }
+}
